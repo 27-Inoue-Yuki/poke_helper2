7d686e0ff74e7fbf55970aaa264ab55a09658ef2
名前 <>
2020-06-12 15:45:57 +0000

    ユーザーページへのルーティング記入

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'users/new'
   get 'pages/index'
   root 'pages#index'
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
