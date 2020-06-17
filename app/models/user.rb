d3b18d5c91f025793e535a2b37fcfc7c61812577
名前 <>
2020-06-17 04:08:23 +0000

    Userモデル:email,:name,:passwordに対してvalidatesを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :name, presence: true,length: { in: 1..15 }
+  SPECIAL_EMAIL = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i.freeze
+  validates :email, presence: true,format:{with: SPECIAL_EMAIL}
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i }
   has_secure_password
 end
